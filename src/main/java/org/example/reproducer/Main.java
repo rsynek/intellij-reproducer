db33ee9b6dc8b6090847af27007162be3e673f0d
Radovan Synek <rsynek@redhat.com>
2021-08-19 13:33:51 +0200

    Initial commit

---
+++ src/main/java/org/example/reproducer/Main.java
@@ -0,0 +1,8 @@
+package org.example.reproducer;
+
+public class Main {
+    public static void main(String[] args) {
+        var someLocalVariable = "nothing important";
+        System.out.println(someLocalVariable);
+    }
+}
